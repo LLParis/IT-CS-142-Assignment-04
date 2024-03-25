e5626d7341e30c6531aa460ae4b90347faef4379
Your Name <you@example.com>
2024-03-24 19:26:38 -0700

    Work In Progress

--- src/GroceryItemOrder.java
+++ src/GroceryItemOrder.java
@@ -1,5 +1,4 @@
 // This class stores information about a single grocery item being ordered.
-
 public class GroceryItemOrder {
     private String name;
     private int quantity;
@@ -13,6 +12,29 @@ public class GroceryItemOrder {
         this.pricePerUnit = pricePerUnit;
     }
 
+    // Changes the quantity of items for this GroceryItemOrder
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 
+    // Returns this GroceryItemOrder's name
+    public String getName() {
+        return name;
+    }
 
+    // Returns this GroceryItemOrder's quantity
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    // Returns this GroceryItemOrder's total cost
+    public double getCost() {
+        return (quantity * pricePerUnit);
+    }
+
+    // Returns a string representation of this GroceryItemOrder specifying the item's name and quantity.
+    // '4 of yogurt'
+    public String toString() {
+        return getQuantity() + " of " + getName();
+    }
 }
